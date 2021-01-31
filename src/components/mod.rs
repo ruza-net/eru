956e0b7874e38f065ffdb5484c28351d51261a2a
Jan Růžička <jan.ruzicka01@gmail.com>
2021-01-31 22:21:14 +0100

    add sidebar component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,6 @@
 mod app;
 
 pub mod general;
+pub mod sidebar;
 
 pub use app::App;
---
+++ src/components/sidebar.rs
@@ -0,0 +1,75 @@
+use crate::components::{
+    general::Tooltip,
+};
+
+use crate::styles::container;
+
+
+
+pub struct Sidebar {
+    width: u16,
+    tools: Vec<Tooltip<Message>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    FreshWorkspace,
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        todo![]
+    }
+}
+
+
+
+impl Default for Sidebar {
+    fn default() -> Self {
+        let mut new_workspace = Tooltip::from_file("res/img/plus");
+        new_workspace.on_press(Message::FreshWorkspace);
+
+        Self {
+            width: style::WIDTH,
+            tools: vec![new_workspace],
+        }
+    }
+}
+
+impl Sidebar {
+    pub fn view(&mut self) -> iced::Element<Message> {
+        let width = self.width;
+
+        let tools = self.tools
+            .iter_mut()
+            .map(|tool| tool.view(Some(width)))
+            .collect();
+
+        iced::Container::new(iced::Column::with_children(tools))
+            .width(iced::Length::Units(self.width + 2 * container::PADDING))
+            .height(iced::Length::Fill)
+            .style(style::Default)
+            .padding(container::PADDING)
+            .into()
+    }
+}
+
+pub mod style {
+    use iced::container;
+
+
+    pub const WIDTH: u16 = 32;
+    pub const BG_SHADE: f32 = 230.;
+
+    pub struct Default;
+
+    impl container::StyleSheet for Default {
+        fn style(&self) -> container::Style {
+            container::Style {
+                background: Some(color![ BG_SHADE; BG_SHADE; BG_SHADE ].into()),
+
+                ..fill![]
+            }
+        }
+    }
+}
